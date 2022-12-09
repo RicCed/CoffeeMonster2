037ffadc59a6c79afe90cfaec687aac4c483634b
RicCed <50809152+RicCed@users.noreply.github.com>
2022-12-09 11:46:42 +0100

    Made the menu page more functional

--- Site/js/script.js
+++ Site/js/script.js
@@ -17,5 +17,23 @@ function goback() {
     document.getElementById("default").classList.add("visible");
     document.getElementById("go-back").classList.add("hidden");
     document.getElementById("go-back").classList.remove("visible");
+  } else if (
+    document.getElementById("personalize").classList.contains("hidden") !== true
+  ) {
+    document.getElementById("personalize").classList.add("hidden");
+    document.getElementById("brewed").classList.remove("hidden");
+    document.getElementById("brewed").classList.add("visible");
   }
 }
+
+function personalize() {
+  if (document.getElementById("personalize").classList.contains("hidden")) {
+    document.getElementById("personalize").classList.remove("hidden");
+    document.getElementById("brewed").classList.remove("visible");
+    document.getElementById("brewed").classList.add("hidden");
+  }
+}
+
+// function size() {
+//   if(document.getElementById("small").classList.contains("")
+// }
